aae81fb36e6512721d4cd98ae99c4e9e150aca76
kaiserk <55062263+kaiserkeenmon@users.noreply.github.com>
2024-03-07 14:42:27 -0600

    adding route file generation progress

--- src/Modernize/CreateServiceCommand.php
+++ src/Modernize/CreateServiceCommand.php
@@ -14,7 +14,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Modernize\Utilities\String;
+use Modernize\Utilities\Strings;
 use WPModernPlugin\Modernize\Traits\PluginDirectory;
 
 class CreateServiceCommand extends Command
@@ -69,7 +69,7 @@ class CreateServiceCommand extends Command
         $repositoryName = preg_replace('/RepositoryInterface$/', 'Repository', $repositoryInterfaceName);
 
         // Convert hyphenated plugin directory name to CamelCase for namespace
-        $namespaceBase = String::hyphenToCamelCase($this->pluginDirName);
+        $namespaceBase = Strings::hyphenToCamelCase($this->pluginDirName);
 
         /**
          * Create the service class.
--- src/Modernize/Utilities/Strings.php
+++ src/Modernize/Utilities/Strings.php
@@ -9,14 +9,14 @@
 
 namespace Modernize\Utilities;
 
-class String {
+class Strings {
     /**
      * Converts a hyphenated string to CamelCase.
      *
-     * @param string $string The hyphenated string.
-     * @return string The CamelCase string.
+     * @param Strings $string The hyphenated string.
+     * @return Strings The CamelCase string.
      */
-    public static function hyphenToCamelCase(string $string): string {
+    public static function hyphenToCamelCase(Strings $string): Strings {
         $str = str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
         return $str;
     }
